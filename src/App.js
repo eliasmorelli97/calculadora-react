6657433f4ea5430408a8755ebc2256d2bd650d28
Elias Morelli <ejmorelli97@gmail.com>
2022-12-14 18:29:28 -0300

    Deploy 3

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Button from './components/Button/Button';
 import Screen from './components/Screen/Screen';
 import ClearButton from './components/ClearButton/ClearButton';
 import { useState, useEffect } from 'react';
+import { BrowserRouter } from 'react-router-dom';
 import { evaluate } from 'mathjs';
 
 function App() {
@@ -51,36 +52,38 @@ function App() {
 
   return (
     <div className='App'>
-      <div className='calculator-container'>
-        <Screen input={input} />
-        <div className='row'>
-          <Button handleClick={addInput}>1</Button>
-          <Button handleClick={addInput}>2</Button>
-          <Button handleClick={addInput}>3</Button>
-          <Button handleClick={addInput}>+</Button>
+      <BrowserRouter basename='/proyecto_react'>
+        <div className='calculator-container'>
+          <Screen input={input} />
+          <div className='row'>
+            <Button handleClick={addInput}>1</Button>
+            <Button handleClick={addInput}>2</Button>
+            <Button handleClick={addInput}>3</Button>
+            <Button handleClick={addInput}>+</Button>
+          </div>
+          <div className='row'>
+            <Button handleClick={addInput}>4</Button>
+            <Button handleClick={addInput}>5</Button>
+            <Button handleClick={addInput}>6</Button>
+            <Button handleClick={addInput}>-</Button>
+          </div>
+          <div className='row'>
+            <Button handleClick={addInput}>7</Button>
+            <Button handleClick={addInput}>8</Button>
+            <Button handleClick={addInput}>9</Button>
+            <Button handleClick={addInput}>*</Button>
+          </div>
+          <div className='row'>
+            <Button handleClick={addInput}>.</Button>
+            <Button handleClick={addInput}>0</Button>
+            <Button handleClick={getResult}>=</Button>
+            <Button handleClick={addInput}>/</Button>
+          </div>
+          <div className='row'>
+            <ClearButton handleClear={clearInput}>Clear</ClearButton>
+          </div>
         </div>
-        <div className='row'>
-          <Button handleClick={addInput}>4</Button>
-          <Button handleClick={addInput}>5</Button>
-          <Button handleClick={addInput}>6</Button>
-          <Button handleClick={addInput}>-</Button>
-        </div>
-        <div className='row'>
-          <Button handleClick={addInput}>7</Button>
-          <Button handleClick={addInput}>8</Button>
-          <Button handleClick={addInput}>9</Button>
-          <Button handleClick={addInput}>*</Button>
-        </div>
-        <div className='row'>
-          <Button handleClick={addInput}>.</Button>
-          <Button handleClick={addInput}>0</Button>
-          <Button handleClick={getResult}>=</Button>
-          <Button handleClick={addInput}>/</Button>
-        </div>
-        <div className='row'>
-          <ClearButton handleClear={clearInput}>Clear</ClearButton>
-        </div>
-      </div>
+      </BrowserRouter>
     </div>
   );
 }
